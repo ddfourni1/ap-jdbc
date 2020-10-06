93efaa51e5bd4f16fb7a2c441c076ea4e06dc3d0
ben-spiller <ben-spiller@users.noreply.github.com>
2020-10-06 15:52:19 +0100

    add a trivial test and workflow

---
+++ test/correctness/JDBC_cor_001/run.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2015-2016, 2018-2020 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors. 
+# Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG 
+
+from pysys.constants import *
+from apama.basetest import ApamaBaseTest
+from apama.correlator import CorrelatorHelper
+
+class PySysTest(ApamaBaseTest):
+
+	def execute(self):
+
+		correlator = CorrelatorHelper(self, name='correlator')
+		correlator.start(logfile='correlator.log')
+		
+		correlator.injectEPL(filenames=['simple.mon'])
+		
+		self.waitForGrep('correlator.log', expr="Loaded simple test monitor", 
+			process=correlator.process, errorExpr=[' (ERROR|FATAL) .*'])
+		
+	def validate(self):
+		# look for log statements in the correlator log file
+		self.assertGrep('correlator.log', expr=' (ERROR|FATAL) .*', contains=False)
